ac945193c906c2d396beb0314416c4bb5faf7156
bertinNDAYISHIMIYE <neddybertin@gmail.com>
2021-04-02 19:04:49 +0200

    ft: product categories

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserRoutes from './user.routes';
+import categoryRoutes from './category.routes.js'
 
 const route = Router();
 
 route.use('/auth', UserRoutes);
+route.use('/category', categoryRoutes)
 
 export default route;
