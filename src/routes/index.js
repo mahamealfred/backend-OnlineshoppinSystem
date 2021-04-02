c29fa3d4c9facf3ebf09d928c7217dd71e5bdbdd
blaise82 <52036624+blaise82@users.noreply.github.com>
2021-04-02 19:08:05 +0200

    commit 1
    commit 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserRoutes from './auth.routes';
+import ProductsRoutes from './products.routes';
 
 const route = Router();
 
 route.use('/auth', UserRoutes);
+route.use('/products', ProductsRoutes);
 
 export default route;
